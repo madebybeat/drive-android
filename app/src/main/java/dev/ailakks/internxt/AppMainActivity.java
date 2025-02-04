76933b52a730b90f56573a29d2f68dcc19bd3fd0
Ailakks <hola@ailakks.dev>
2025-02-04 21:49:10 +0100

    feat: main activity

---
+++ app/src/main/java/dev/ailakks/internxt/AppMainActivity.java
@@ -0,0 +1,16 @@
+package dev.ailakks.internxt;
+
+import android.os.Bundle;
+
+import com.beatstreaming.core.MainActivity;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class AppMainActivity extends MainActivity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+    }
+}
---
+++ app/src/main/java/dev/ailakks/internxt/InternxtApp.java
@@ -0,0 +1,9 @@
+package dev.ailakks.internxt;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class InternxtApp extends Application {
+}
